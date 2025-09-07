02cedb09dec4e58d93439ab82c8d75542871e399
RichardKaranuMbuti <officialforrichardk@gmail.com>
2025-09-08 01:56:30 +0300

    first commit

---
+++ src/main/java/com/example/authbackend/AuthbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.authbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthbackendApplication.class, args);
+	}
+
+}
